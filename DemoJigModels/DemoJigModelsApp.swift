d3f7354db489312e9298f32cd713c9fb5faae605
sonpt61 <sonpt61@fpt.com>
2024-08-28 09:15:58 +0700

    Initial Commit

---
+++ DemoJigModels/DemoJigModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoJigModelsApp.swift
+//  DemoJigModels
+//
+//  Created by SonPT61 on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoJigModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
